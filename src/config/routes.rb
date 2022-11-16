19a2b38e792afde972105894ca00582372bb3ae0
M-yuk1ko <m.develop.yukiko091029@gmail.com>
2022-11-16 11:55:24 +0900

    CIエラー対応のためrails_admin導入＋各設定

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  #rails_admin導入で追加▼
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
   get '/', to: 'users#index'
   # Defines the root path route ("/")
   # root "articles#index"
